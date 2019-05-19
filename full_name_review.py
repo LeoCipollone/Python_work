9047c6f0452b18139cf83496c9870da297c8936f
LeoCipollone <leocipollone@outlook.com>
2019-05-19 20:18:06 -0300

    Fifth program review

---
+++ full_name_review.py
@@ -0,0 +1,16 @@
+first_name = "leo"
+last_name = "cipollone"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+
+first_name = "pepito"
+last_name = "grillo"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+first_name = "JuAn"
+last_name = "SANCHEZ"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
