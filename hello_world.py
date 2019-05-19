533215671445c8cff75b169d4727fc774300d006
LeoCipollone <leocipollone@outlook.com>
2019-05-19 02:40:48 -0300

    First program

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print("Hello Python world!")
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+mensaje = "Hola"
+print(mensaje)
+
+papas = "Fritas"
+print(papas)
+
+hola = "¿Cómo estás?"
+print(hola)
