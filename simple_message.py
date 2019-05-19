d1b9c0994d09c4431e7fa63a7059a0dd4991a4f6
LeoCipollone <leocipollone@outlook.com>
2019-05-19 02:44:53 -0300

    Second program

---
+++ simple_message.py
@@ -0,0 +1,2 @@
+mensaje = "Prueba numero uno"
+print(mensaje)
---
+++ simple_messages.py
@@ -0,0 +1,5 @@
+mensaje = "lo que va entre comillas se llama string"
+print(mensaje)
+
+mensaje_2 = 'esto tambien se llama string'
+print(mensaje_2)
