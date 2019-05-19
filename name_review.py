5021b44faabc41e3ba9d45ab7055ec234cc8c736
LeoCipollone <leocipollone@outlook.com>
2019-05-19 20:17:13 -0300

    Fourth program review

---
+++ name_review.py
@@ -0,0 +1,14 @@
+name = "ada lovelace"
+print(name.title())
+
+name = "leo cipollone"
+print(name.title())
+
+name = "pepito grillo"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+frase = "el día de hoy veremos los strings"
+print(frase.title())
+print(frase.upper())
