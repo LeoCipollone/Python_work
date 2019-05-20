7b23fefe81b0e1ffa079544f01ed15457b925af9
LeoCipollone <leocipollone@outlook.com>
2019-05-20 02:21:07 -0300

    Syntax errors apostrophes

---
+++ apostrophe.py
@@ -0,0 +1,5 @@
+message = "One of Python's strengths is it's diverse community"
+print(message) #Si se va a usar un apostrofe empezar con comillas
+
+message = 'Pepe le dijo "si vas a salir que sea rápido"'
+print(message) #Si se van a usar comillas empezar con apostrofe
