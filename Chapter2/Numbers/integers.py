1f48577b5a506d88375414bcb5040d03bb381d1b
LeoCipollone <leocipollone@outlook.com>
2019-05-25 20:34:48 -0300

    Separación por carpetas

---
+++ Chapter2/Numbers/integers.py
@@ -0,0 +1,9 @@
+2 + 2 #No muestra el resultado
+print(3 ** 2) #Muestra el resultado
+print("3 ** 2") #Muestra exactamente lo escrito
+
+2 + 3 #Suma
+3 - 2 #Resta
+2 * 3 #Multiplicación
+3 / 2 #División
+2 ** 3 #Exponentes
